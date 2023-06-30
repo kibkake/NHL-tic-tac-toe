dde363095e74540e369698568db58ce89ecd74ec
kibkake <kirby.fung@gmail.com>
2023-06-30 13:57:43 -0700

    initial commit

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.css']
+})
+export class SquareComponent {
+
+  rando = Math.random();
+}
